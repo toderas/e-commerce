0737657cf72da0144f04ff9d8c0f25838d64d4ba
toderas <toaderchirila21@gmail.com>
2019-07-22 19:47:04 +0000

    Added styles

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -127,8 +127,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = "/media/"
+MEDIA_URL = '/media/'
+
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault("STRIPE_PUBLISHABLE", "pk_test_niC2LQVKaPKZq0iM0pqAGOY100FLsvBXLz")
+os.environ.setdefault("STRIPE_SECRET", "sk_test_qkctMNDEBprxpZIZVUfwwIjZ00cZMftPPw")
